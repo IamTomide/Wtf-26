0982a836fa8b5d2877842fc6d833b0adc70f25fc
Olaleye Oluwatomide <paulinaebunoluwa@gmail.com>
2025-09-07 21:54:57 +0100

    Adding assignment and classwork

---
+++ wtfassignment2/assignment.js
@@ -0,0 +1,10 @@
+let num1 = 24;
+let num2 = 4;
+let name = "Paulina";
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+
+console.log(`Hello ${name}, the result of num1 + num2 is ${num1 + num2}`);
